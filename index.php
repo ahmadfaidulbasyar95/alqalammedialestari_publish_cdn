cd0fca15cec9147aecfc3ab4e612d90454f62468
Melly-Faid <ahmadfaidulbasyar95@gmail.com>
2025-04-23 21:26:18 +0700

    fix info

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 $FILES = [
-	'mod/doc/info.js',
+	'mod/doc/info_.js',
 	'mod/doc/load_.js',
 	'mod/doc/modern-screenshot.min.js',
 	'mod/doc/qrcode.min.js',
--- mod_doc_info_.js
+++ mod_doc_info_.js
@@ -5,7 +5,7 @@
 			post_labels.children('a').each(function(k,v) {
 				if ($(v).text() == 'Buku') {
 					var url = window.__doc_info_url+'?url='+encodeURIComponent(window.location.origin+window.location.pathname);
-					var html = '<div class="text-center" style="margin: 30px 0;">'+
+					var html = '<div class="text-center post-doc_info" style="margin: 30px 0;">'+
 							'<h3><i class="fa fa-download fa-fw"></i> Download</h3>'+
 							'<a href="'+url+'" target="_BLANK" class="btn btn-success btn-md"><i class="fa fa-id-card-o fa-fw"></i> Sertifikat</a>'+
 							'<a href="'+url+'" target="_BLANK" class="btn btn-info btn-md" style="margin-left: 5px;"><i class="fa fa-calendar-check-o fa-fw"></i> SK Terbit</a>'+
